e6c85319c314dedbdf7bbdbdd67a6a8a1b7fcab7
Ashwani Sharma <ashwani.sh99@gmail.com>
2024-05-10 11:10:30 +0000

    Deployment Completed

---
+++ app/frontedstu/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","com/ingex/frontedstu/model/models"],function(e,t,i){"use strict";return e.extend("com.ingex.frontedstu.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
